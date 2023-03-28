16f7def732f4d7d7df99be353bb7d897b7c5c9b4
jsherfey-nonemp <jsherfey@collegeboard.org>
2023-03-27 20:42:37 -0700

    Initial Commit

---
+++ ios-AR-maggie/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ios-AR-maggie
+//
+//  Created by Sherfey, Justin-NONEMP on 3/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-AR-maggie/ios_AR_maggieApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_AR_maggieApp.swift
+//  ios-AR-maggie
+//
+//  Created by Sherfey, Justin-NONEMP on 3/27/23.
+//
+
+import SwiftUI
+
+@main
+struct ios_AR_maggieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
